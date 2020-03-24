c7ac47f590d66fc596478d31ab5b18de3022ec28
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-03-24 03:09:56 +0000

    Add mock test for axios

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,37 @@
 import React from "react";
-
-import { render, cleanup } from "@testing-library/react";
+import axios from "__mocks__/axios";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+  let days, appointments, interviewers;
+  Promise.all([
+    Promise.resolve(axios.get("/api/days")),
+    Promise.resolve(axios.get("/api/appointments")),
+    Promise.resolve(axios.get("/api/interviewers"))
+  ]).then(all => {
+    (days = all[0].data),
+      (appointments = all[1].data),
+      (interviewers = all[2].data);
+  });
+
+  const { getByText } = render(
+    <Application
+      days={days}
+      appointments={appointments}
+      interviewers={interviewers}
+    />
+  );
+
+  await waitForElement(() => getByText("Monday"));
+  fireEvent.click(getByText("Tuesday"));
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
